3da4ee3afa2a4174aed68a1b44cba128627e902e
jcxiong <jchxiong@gmail.com>
2020-12-29 19:19:24 +0800

    use RCVTIMEO instead of Poller for infserver api

--- tleague/inference_server/api.py
+++ tleague/inference_server/api.py
@@ -8,11 +8,11 @@ class InfServerAPIs(object):
   def __init__(self, inference_server_addr, ds, compress=False, timeout=30000):
     self._zmq_context = zmq.Context()
     ip_ports = list(inference_server_addr.split(','))
+    self.server_addr = inference_server_addr
     random.shuffle(ip_ports)
     self._ip_ports = ip_ports
     self.ds = ds
     self._compress = compress
-    self.poll = zmq.Poller()
     self.timeout = timeout
     self._req_socket = None
     self._rebuild_socket()
@@ -21,11 +21,10 @@ class InfServerAPIs(object):
     if self._req_socket is not None:
       self._req_socket.setsockopt(zmq.LINGER, 0)
       self._req_socket.close()
-      self.poll.unregister(self._req_socket)
     self._req_socket = self._zmq_context.socket(zmq.REQ)
+    self._req_socket.setsockopt(zmq.RCVTIMEO, self.timeout)
     for ip_port in self._ip_ports:
       self._req_socket.connect("tcp://{}".format(ip_port))
-    self.poll.register(self._req_socket, zmq.POLLIN)
 
   def request_output(self, obs):
     # obs must match the data structure defined in self.ds
@@ -36,13 +35,12 @@ class InfServerAPIs(object):
       data = pickle.dumps(data)
     self._req_socket.send(data)
     while True:
-      socks = dict(self.poll.poll(self.timeout))
-      if socks.get(self._req_socket) == zmq.POLLIN:
+      try:
         ret = self._req_socket.recv_pyobj()
         break
-      else:
-        print(f'Timeout ({self.timeout} ms) for request inference service,'
-              f' restart a socket and try again!')
+      except Exception as e:
+        print(f'Exception:{e} After {self.timeout} ms for request inference '
+              f'service {self.server_addr}, restart a socket and try again!')
         self._rebuild_socket()
         self._req_socket.send(data)
     return ret
